9fe9da5c7356cb70f9a49f8201d6b722e26ba4b0
adithep <adtp2221@gmail.com>
2020-08-18 20:59:54 -0700

    refactor

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -11,83 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    private var isFinishedTyping = true
-    private var isDecimal = false
-    private var numbers = 0.0
-    private var symbol = ""
+    
+    var calculate = CalculatorLogic()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculate.label = self.displayLabel
         
-    }
-    
-    func resetNumber() {
-        displayLabel.text = "0"
-        isFinishedTyping = true
-        isDecimal = false
     }
     
     @IBAction func functionButtonPressed(_ sender: UIButton) {
         if let function = sender.currentTitle {
-            switch function {
-            case "AC":
-                resetNumber()
-                numbers = 0.0
-            case "+/-":
-                displayLabel.text = String(Double(displayLabel.text!)! * -1)
-            case "%":
-                displayLabel.text = String(Double(displayLabel.text!)! * 0.01)
-            case "+", "-","÷","×":
-                numbers = Double(displayLabel.text!)!
-                resetNumber()
-                switch function {
-                case "+":
-                    symbol = "+"
-                case "-":
-                    symbol = "-"
-                case "÷":
-                    symbol = "÷"
-                case "×":
-                    symbol = "×"
-                default:
-                    print("Something's wrong from calculation")
-                }
-            case "=":
-                switch symbol {
-                case "+":
-                    numbers += Double(displayLabel.text!)!
-                case "-":
-                    numbers -= Double(displayLabel.text!)!
-                case "÷":
-                    numbers /= Double(displayLabel.text!)!
-                case "×":
-                    numbers *= Double(displayLabel.text!)!
-                default:
-                    print("Something's wrong from =")
-                }
-                displayLabel.text = String(format: "%.2f", numbers)
-            default:
-                print("Something")
-            }
+            calculate.checkFunction(symbols: function)
         }
     }
     
     
     @IBAction func numberButtonPressed(_ sender: UIButton) {
         if let number = sender.currentTitle {
-            if number == "." && !isDecimal {
-                displayLabel.text = displayLabel.text! + number
-                isDecimal = true
-            } else if number == "." && isDecimal {
-                return
-            } else {
-                if isFinishedTyping {
-                    displayLabel.text = number
-                    isFinishedTyping = false
-                } else {
-                    displayLabel.text = displayLabel.text! + number
-                }
-            }
+            calculate.checkNumber(num: number)
         }
     }
     
